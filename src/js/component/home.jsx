e814804e55a1924d80405dbbe175daf6521ca536
velazcopolo <144776724+velazcopolo@users.noreply.github.com>
2023-12-04 19:44:59 +0000

    to do list funcionando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
+
 const Home = () => {
+
+	const [add, setAdd] = useState("");
+	const [list, setList] = useState([]);
+	const [hoverIndex, setHoverIndex] = useState("");
+	function keyIntro(e) {
+
+		// e.preventDefault();
+		// console.log(e.keyCode);
+		if (e.keyCode === 13) {
+			setList(list.concat(add))
+			setAdd("")
+
+		}
+
+	};
+
+	function removeItem(index) {
+		const updatedList = [...list];
+		updatedList.splice(index, 1);
+		setList(updatedList);
+	}
+
+
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center container mt-5">
+
+			<div className="mb-3 ">
+
+				<input type="text" className="form-control" value={add} placeholder="add a element" onChange={(e) => setAdd(e.target.value)} onKeyDown={keyIntro} />
+
+			</div>
+
+			<div>
+				<ul>
+					{list.map((item, index) =>
+						<li
+							className="d-flex justify-content-between"
+							key={index}
+							onMouseEnter={() => setHoverIndex(index)}
+							onMouseLeave={() => setHoverIndex(null)}
+						>
+							{item}
+							{hoverIndex === index && (
+								<button onClick={() => removeItem(index)}>X</button>
+							)}
+						</li>
+					)}
+				</ul>
+			</div>
+
 		</div>
 	);
+
 };
 
 export default Home;
